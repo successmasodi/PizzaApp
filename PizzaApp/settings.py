7332a66d6d29f2e388450559f8731be4c9e14f73
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-10-03 13:49:44 +0100

    added postgresql database connected to dicitalocean

--- PizzaApp/settings.py
+++ PizzaApp/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'PizzaApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -103,6 +103,19 @@ DATABASES = {
 #         'PORT': os.getenv("PORT"),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+                'NAME': os.getenv("NAME"),
+                'USER': os.getenv("USER"),
+                'PASSWORD': os.getenv("DB_PASSWORD"),
+                'HOST': os.getenv("HOST"),
+                'PORT': os.getenv("PORT"),
+
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
